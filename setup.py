feacfaaec7f897bed32423269d0e2863377e7b62
PablitoBueno <pablobuenodearaujo@gmail.com>
2024-11-24 16:28:34 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from Cython.Build import cythonize
 
 setup(
       ext_modules = cythonize(
-        ["a_star.pyx", "greedy_search.pyx","hill_climbing.pyx"], language="c++"  # Especifica C++ como o compilador
+        ["a_star.pyx", "greedy_search.pyx","hill_climbing.pyx","s_annealing.pyx","ant_colony.pyx"], language="c++"  # Especifica C++ como o compilador
       )
 )
